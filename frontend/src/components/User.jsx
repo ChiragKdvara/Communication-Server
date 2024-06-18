58d75a3238025f301c0ebf054dd3df44d8435c27
26sameer <sameer26basha@gmail.com>
2024-06-18 14:44:50 +0530

    Updated useEffect function calling an API

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 const User = () => {
   const BASE_URL = import.meta.env.VITE_URL
   const location = useLocation()
-  const user_data = location.state
+  const user_data = location?.state
   const navigate = useNavigate()
 
   const [expMsgData, setExpMsgData] = useState([])
@@ -14,7 +14,7 @@ const User = () => {
   useEffect(() => {
     const fetchReferenceData = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/api/v1/users?username=${user_data.username}`)
+        const response = await axios.get(`${BASE_URL}/api/v1/users?username=${user_data?.username}`)
         let data = response.data.exp_messages
         console.log(data)
         setExpMsgData(data)
@@ -34,18 +34,18 @@ const User = () => {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">Hey, {user_data.username}</h1>
+      <h1 className="text-2xl font-bold">Hey, {user_data?.username}</h1>
 
       {loading ? ( // Show loading message while fetching data
         <p>Loading...</p>
-      ) : expMsgData.length > 0 ? (
+      ) : expMsgData?.length > 0 ? (
         <div className="w-2/3">
           <h2 className="text-xl font-semibold">Messages:</h2>
-          {expMsgData.map((exp) => (
-            <div key={exp.id} onClick={() => handleClick(exp.id)}>
+          {expMsgData?.map((exp) => (
+            <div key={exp.id} onClick={() => handleClick(exp?.id)}>
               <div className="flex justify-between bg-primary my-3 px-4 rounded-md cursor-pointer">
-                <p>{exp.msg_title}</p>
-                <p>{exp.sent_time}</p>
+                <p>{exp?.msg_title}</p>
+                <p>{exp?.sent_time}</p>
               </div>
             </div>
           ))}
